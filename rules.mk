ed5d96285d1ad216926d68fbf54be68edb9eb955
Diego Martinez <degorckterr@gmail.com>
2024-11-05 10:43:46 -0600

    Updated rules mk to fit with new qmk version

--- rules.mk
+++ rules.mk
@@ -3,6 +3,6 @@ RGB_MATRIX_ENABLE  = yes
 MOUSEKEY_ENABLE    = no
 NKRO_ENABLE        = yes
 OLED_ENABLE        = yes
-OLED_DRIVER        = SSD1306
+OLED_DRIVER        = ssd1306
 LTO_ENABLE         = yes
 WPM_ENABLE = yes
